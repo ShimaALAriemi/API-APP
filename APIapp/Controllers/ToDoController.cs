3f4a484ae9a0e3e1cf5bb7ab4d455f8d438a7764
Shaima <shooomi9988@gmail.com>
2024-01-29 01:01:05 +0400

    Update CRUD

--- APIapp/Controllers/ToDoController.cs
+++ APIapp/Controllers/ToDoController.cs
@@ -1,6 +1,7 @@
 ﻿using APIapp.Context;
 using APIapp.Model;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIapp.Controllers
@@ -19,7 +20,7 @@ namespace APIapp.Controllers
         [ProducesResponseType(statusCode:200)]
         [ProducesResponseType(statusCode:404)]
 
-        public ActionResult GetAll()
+        public ActionResult<IEnumerable<ToDo>> GetAll()
         {
             var todos = _dbContext.ToDoList.ToList();
             if (todos.Count > 0)
@@ -41,17 +42,44 @@ namespace APIapp.Controllers
         [HttpGet("id:int")]
         [ProducesResponseType(statusCode: 200)]
         [ProducesResponseType(statusCode: 404)]
-        public ActionResult Get(int id)
+        [ProducesResponseType(statusCode: 400)]
+
+        public ActionResult<ToDo> Get(int id)
         {
-            var todo = _dbContext.ToDoList.Find(id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+             var todo = _dbContext.ToDoList.Find(id);
+            
+            if (todo == null) 
+                {
+                return NotFound();
+                }
             return Ok(todo);
+
             //return NotFound();
         }
 
         [HttpPost]
-        
+        [ProducesResponseType(statusCode: 200)]
+        [ProducesResponseType(statusCode: 404)]
+        [ProducesResponseType(statusCode: 400)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
         public ActionResult Create(ToDo toDo)
         {
+            if (toDo == null)
+            {
+                return BadRequest(toDo);
+            }
+
+            if (toDo.ID > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
+
             if (ModelState.IsValid)
             {
                 _dbContext.ToDoList.Add(toDo);
@@ -61,7 +89,10 @@ namespace APIapp.Controllers
             return BadRequest();
         }
 
-        [HttpPost("id:int")]
+        [HttpDelete("id:int")]
+        [ProducesResponseType(statusCode: 200)]
+        [ProducesResponseType(statusCode: 404)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult Delete(int id)
         {
             if(id == 0)
@@ -72,21 +103,55 @@ namespace APIapp.Controllers
             if (taskDone != null) {
                 _dbContext.ToDoList.Remove(taskDone);
                 _dbContext.SaveChanges();
-                return Ok(taskDone);
+                return NoContent();
             }
             return BadRequest();
         }
 
-        //[HttpPost]
-        //public ActionResult Update(ToDo toDo)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _dbContext.ToDoList.Update(toDo);
-        //        _dbContext.SaveChanges();
-        //        return Ok(toDo);
-        //    }
-        //    return BadRequest();
-        //}
+        [HttpPut("id:int")]
+        [ProducesResponseType(statusCode: 404)]
+        [ProducesResponseType(statusCode: 400)]
+        public ActionResult Update(int id, ToDo toDo)
+        {
+            if (id == 0 || id != toDo.ID || toDo == null)
+            {
+                return BadRequest();
+            }
+            var taskUpdated = _dbContext.ToDoList.Where(o => o.ID == id).FirstOrDefault();
+
+            if (taskUpdated != null) if (ModelState.IsValid)
+            {
+                    taskUpdated.Name = toDo.Name;
+                    taskUpdated.Description = toDo.Description;
+                    
+
+                _dbContext.ToDoList.Update(taskUpdated);
+                _dbContext.SaveChanges();
+                    return NoContent();
+                }
+            return BadRequest();
+        }
+
+        [HttpPatch("id:int")]
+        [ProducesResponseType(statusCode: 404)]
+        [ProducesResponseType(statusCode: 400)]
+        public ActionResult Update(int id, JsonPatchDocument<ToDo> document)
+        {
+            if (id == 0 || document == null)
+            {
+                return BadRequest();
+            }
+
+            var taskUpdated = _dbContext.ToDoList.Where(o => o.ID == id).FirstOrDefault();
+            if (taskUpdated != null)
+            {
+                document.ApplyTo(taskUpdated, ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return NoContent();
+        }
     }
 }
--- APIapp/Program.cs
+++ APIapp/Program.cs
@@ -16,7 +16,7 @@ namespace APIapp
                 options => options.UseSqlServer(
                 builder.Configuration.GetConnectionString("Connection")));
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddNewtonsoftJson();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
